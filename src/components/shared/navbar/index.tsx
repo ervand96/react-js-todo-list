9c37e93b5f275782791d2d9c15f7afcc483094c9
ervand96 <ervandharutyunyan06096@gmail.com>
2022-09-27 06:39:30 -0800

    change

--- src/components/shared/navbar/index.tsx
+++ src/components/shared/navbar/index.tsx
@@ -1,4 +1,4 @@
-import { navbar, NavbarProps } from "../../constants/navbar";
+import { navbar, NavbarProps } from "../../../constants/navbar";
 import { Link } from "react-router-dom";
 import styles from "./navbar.module.scss";
 
