960fc0b129085557485e5352bc068212483f49c8
ervand96 <ervandharutyunyan06096@gmail.com>
2022-11-14 02:10:22 -0800

    create

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+import FAKE_DATA from '../../constants/MOCK_DATA.json'
 import styles from "./about.module.scss";
 
 export default function About(): JSX.Element {
+  const [data, setData] = useState<string>('');
+
+  const getValue = (e: any): void => {
+    setData(e.target.value);
+  }
+
   return (
     <div className={styles.container}>
-      <h1>About Page</h1>
+      <div className={styles.section}>
+
+        <input placeholder="Search Data..." onChange={event => { getValue(event) }} />
+        {FAKE_DATA.filter((val) => {
+          if (data.trim() == '') {
+            return val
+          } else if (val.first_name.toLocaleLowerCase().includes(data.toLocaleLowerCase())) {
+            return val
+          }
+        }).map((e) => (
+          <div className={styles.data}>
+            <p>{e.first_name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
